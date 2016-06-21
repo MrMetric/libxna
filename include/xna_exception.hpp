9a1a3b74f0490aa74137d5044540b4344d2b71d2
Andrew Harrison <gamefreak1974@gmail.com>
2016-06-20 22:36:07 -0700

    fixed warning about no out-of-line virtual thingies

--- include/xna_exception.hpp
+++ include/xna_exception.hpp
@@ -8,6 +8,8 @@ class xna_error : public std::runtime_error
 	public:
 		explicit xna_error(const std::string& what_arg);
 		explicit xna_error(const char* what_arg);
+
+		virtual void dummy();
 };
 
 class lzx_error : public xna_error
@@ -15,4 +17,6 @@ class lzx_error : public xna_error
 	public:
 		explicit lzx_error(const std::string& what_arg);
 		explicit lzx_error(const char* what_arg);
+
+		virtual void dummy();
 };
--- src/xna_exception.cpp
+++ src/xna_exception.cpp
@@ -5,6 +5,8 @@ using std::string;
 
 xna_error::xna_error(const string& what_arg) : runtime_error(what_arg) {}
 xna_error::xna_error(const char* what_arg) : runtime_error(what_arg) {}
+void xna_error::dummy(){}
 
 lzx_error::lzx_error(const string& what_arg) : xna_error(what_arg) {}
 lzx_error::lzx_error(const char* what_arg) : xna_error(what_arg) {}
+void lzx_error::dummy(){}
